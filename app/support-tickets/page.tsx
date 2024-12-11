4b6ce5edbe70dcf972fb63eaed04786dab968535
Jimmy091286 <jimmy091286@example.com>
2024-12-11 18:10:04 +0100

    Barrierefreiheit der Support-Tickets verbessert

---
+++ app/support-tickets/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { SupportTickets } from '@/components/SupportTickets'
+
+export default function SupportTicketsPage() {
+  return (
+    <main>
+      <title>Support Tickets</title>
+      <SupportTickets />
+    </main>
+  )
+}
+
